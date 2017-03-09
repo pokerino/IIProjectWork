35d9e78495234585786247002a1f26c8d1a6fff7
pokerino <shadowpok3r@gmail.com>
2017-03-09 15:08:39 +0100

    Starting Project
    Just bases for project.

---
+++ IIProjectClient/Models/FordonPassage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IIProjectClient.Models
+{
+    public class FordonPassage
+    {
+        string fordonEpc { get; set; }
+        string platsEpc { get; set; }
+        DateTime tid { get; set; }
+        string plats { get; set; }
+        long evn { get; set; }
+        string fordonsinnehavaren { get; set; }
+        string undershallsansvarigtForetag { get; set; }
+        string fordonsTyp { get; set; }
+        bool giltigtGodkannande { get; set; }
+        DateTime godkannandeStarttid { get; set; }
+        DateTime godkannandeSluttid { get; set; }
+    }
+}
