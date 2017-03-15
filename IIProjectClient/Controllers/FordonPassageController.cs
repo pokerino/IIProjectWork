ed383a3678679cf0838c9a5316a20648d9274fb2
pokerino <shadowpok3r@gmail.com>
2017-03-15 11:45:29 +0100

    Controller
    Bara skapat en FordonPassageController.cs, inte fyllt den med något.

---
+++ IIProjectClient/Controllers/FordonPassageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IIProjectClient.Controllers
+{
+    public class FordonPassageController : Controller
+    {
+        // GET: FordonPassage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
